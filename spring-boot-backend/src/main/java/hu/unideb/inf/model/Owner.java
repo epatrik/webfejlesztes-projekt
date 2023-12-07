7dfa4779e5b596041272bf3f94cd509f2c7247a5
Eperjesi Patrik <epatrik2002@gmail.com>
2023-12-07 22:38:20 +0100

    database models created

--- spring-boot-backend/src/main/java/hu/unideb/inf/model/Owner.java
+++ spring-boot-backend/src/main/java/hu/unideb/inf/model/Owner.java
@@ -1,4 +1,59 @@
 package hu.unideb.inf.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "owners")
 public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Owner(){
+
+    }
+
+    public Owner(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- spring-boot-backend/src/main/java/hu/unideb/inf/model/Pet.java
+++ spring-boot-backend/src/main/java/hu/unideb/inf/model/Pet.java
@@ -1,4 +1,64 @@
 package hu.unideb.inf.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "pets")
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "species")
+    private String species;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Owner owner;
+
+    public Pet() {
+
+    }
+
+    public Pet(String name, String species, Owner owner) {
+        this.name = name;
+        this.species = species;
+        this.owner = owner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
 }
