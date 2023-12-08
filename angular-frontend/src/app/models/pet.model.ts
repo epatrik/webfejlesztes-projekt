36ed01937803760ea4b03940059b42874cdc02a2
Eperjesi Patrik <epatrik2002@gmail.com>
2023-12-08 15:54:25 +0100

    pet model and service added

---
+++ angular-frontend/src/app/models/pet.model.ts
@@ -0,0 +1,6 @@
+export class Pet {
+  id?: any;
+  name?: string;
+  species?: string;
+  owner?: any;
+}
---
+++ angular-frontend/src/app/services/pet.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pet } from '../models/pet.model';
+
+const basePetUrl = 'http://localhost:9090/api/pets';
+const baseOwnerUrl = 'http://localhost:9090/api/owners'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PetService {
+  constructor(private http: HttpClient) {}
+
+  getAllOfOwner(ownerId: any): Observable<Pet[]> {
+    return this.http.get<Pet[]>(`${baseOwnerUrl}/${ownerId}/pets`);
+  }
+
+  get(id: any): Observable<Pet> {
+    return this.http.get<Pet>(`${basePetUrl}/${id}`);
+  }
+
+  create(ownerId: any,data: any): Observable<any> {
+    return this.http.post(`${baseOwnerUrl}/${ownerId}/pets`, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${basePetUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${basePetUrl}/${id}`);
+  }
+
+  deleteAllOfOwner(ownerId: any): Observable<any> {
+    return this.http.delete(`${baseOwnerUrl}/${ownerId}/pets`);
+  }
+}
